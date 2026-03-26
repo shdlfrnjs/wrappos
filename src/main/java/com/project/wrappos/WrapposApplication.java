ebb285fcf28c12cae4a24e62cefa4d4aff1148a5
shdlfrnjs <ilkwon232@gmail.com>
2026-03-26 11:47:11 +0900

    초기 커밋

---
+++ src/main/java/com/project/wrappos/WrapposApplication.java
@@ -0,0 +1,13 @@
+package com.project.wrappos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrapposApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WrapposApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/wrappos/WrapposApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.wrappos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrapposApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
