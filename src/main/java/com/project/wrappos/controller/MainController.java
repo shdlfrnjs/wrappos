8c010c8ceae1cf59e0a043284c30a66161e67721
shdlfrnjs <ilkwon232@gmail.com>
2026-04-09 17:04:17 +0900

    20260409 레이아웃 구조 생성

--- src/main/java/com/project/wrappos/controller/MainController.java
+++ src/main/java/com/project/wrappos/controller/MainController.java
@@ -1,13 +1,20 @@
 package com.project.wrappos.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String home() {
+    public String login() {
         return "login";
+    }
+
+    @GetMapping({"/main"})
+    public String dashboard(Model model) {
+        model.addAttribute("pageTitle", "Dashboard v3");
+        return "pages/dashboard";
     }
 }
